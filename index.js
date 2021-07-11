3d92bad4254b8ecccddea2fabeff510b7d4e9896
Ahmad Kane <82630118+ahmadkane93@users.noreply.github.com>
2021-07-11 17:18:37 -0400

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-
+const employee= [];
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const fs = require('fs');
+const fs = require("fs");
+
 
 
 const questions = [
